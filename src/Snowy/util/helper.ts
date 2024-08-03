2b7f0493210f159cc4c98a395bde9f7074e6c6fd
S.Watanabe <ocelot2828@gmail.com>
2024-08-03 20:08:01 +0900

    fix(helper.ts): Scale and opacity now also accept number input.

--- src/Snowy/util/helper.ts
+++ src/Snowy/util/helper.ts
@@ -6,7 +6,7 @@ export const convertStyles = (sx: CustomCSSProperties | undefined) => {
   const kebabStyles: { [key: string]: PropertyValue } = {}
   for (const key in sx) {
     if (sx.hasOwnProperty.call(sx, key)) {
-      kebabStyles[toKebabCase(key)] = (sx as PropertyType)[key]
+      kebabStyles[toKebabCase(key)] = (sx as PropertyType)[key].toString()
     }
   }
   return kebabStyles
